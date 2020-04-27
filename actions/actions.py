ac48a137fdbee2e079bb5510b4b571ee6ce373f1
melindaloubser1 <melinda.loubser@gmail.com>
2020-04-27 10:25:45 -0400

    nlu label

--- actions/actions.py
+++ actions/actions.py
@@ -1,5 +1,4 @@
 from typing import Dict, Text, Any, List, Union, Optional
-##
 from rasa_sdk import Tracker 
 from rasa_sdk.executor import CollectingDispatcher 
 from rasa_sdk.forms import FormAction 
