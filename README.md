8f83bce32fa0396052b46fd0de30e319c8bba570
hsm207 <hsm207@users.noreply.github.com>
2021-07-23 14:12:07 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Introduction
+
+This repo demonstrates how to build a input validation loop using [Rules](https://rasa.com/docs/rasa/rules).
+
+For more complex use cases, consider using [Forms](https://rasa.com/docs/rasa/forms) with a [custom action to validate the extracted input](https://rasa.com/docs/rasa/forms#validating-form-input). You can break out of a form's loop by returning the [ActionExecutionRejected](https://rasa.com/docs/rasa/forms#writing-stories--rules-for-unhappy-form-paths) event in the form's validation logic.
+
+# Demo
+
+The bot will repeatedly ask the user for a number between 1 and 10.
+
+It will print the number the user gave.
+
+If the user fails to provide a valid number 3 times, then the bot will move on to something else.
+
+![](https://i.ibb.co/xzSSjKc/demo-loop.png)
--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,43 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
+
+import logging
+
+class ActionValidateUserInput(Action):
+    MAX_RETRIES = 3
+
+    def name(self) -> Text:
+        return "action_validate_user_input"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        user_input = int(tracker.latest_message["text"])
+        num_retries = tracker.get_slot("num_retries")
+
+        logging.info(f"Num retries is {num_retries}")
+
+        # validation logic here
+        if user_input >= 1 and user_input <= 10:
+            
+            return [SlotSet("num_retries", 0), SlotSet("user_number", user_input), SlotSet("is_validated", True)]
+
+        elif num_retries <= self.MAX_RETRIES:
+            
+            dispatcher.utter_message(template="utter_try_again")
+            
+            return [SlotSet("num_retries", num_retries + 1)]
+
+        else:
+            return [SlotSet("is_retry_limit", True)]
+
+        
+
+        return []
