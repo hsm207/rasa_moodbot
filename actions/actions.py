799de2df1dc5a6e7bd849bbb4e41d99b02ba9866
hsm207 <hsm207@users.noreply.github.com>
2021-08-12 21:18:26 +0000

    Reformat

--- actions/actions.py
+++ actions/actions.py
@@ -15,35 +15,50 @@ from rasa_sdk.executor import CollectingDispatcher
 
 
 class ActionHelloWorld(Action):
-
     def name(self) -> Text:
         return "action_hello_world"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
 
         dispatcher.utter_message(response="utter_greet_with_name")
 
         return [SlotSet("is_logged_in", True)]
 
+
 class ActionManyEvents(Action):
     def name(self) -> Text:
         return "action_return_many_events"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
 
-        return [SlotSet("is_logged_in", True), SlotSet("name", "John"), Restarted(), AllSlotsReset()]
-        
+        return [
+            SlotSet("is_logged_in", True),
+            SlotSet("name", "John"),
+            Restarted(),
+            AllSlotsReset(),
+        ]
+
+
 class ActionAuthrorizeUser(Action):
     def name(self) -> Text:
         return "action_authorize_user"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
 
         username = tracker.slots["name"]
         if username == "John":
@@ -51,17 +66,21 @@ class ActionAuthrorizeUser(Action):
         else:
             return [SlotSet("is_logged_in", False)]
 
+
 class ActionAgentAvailable(Action):
     def name(self) -> Text:
         return "action_agent_available"
-    
+
     def _is_agent_available(self):
         # call backend to check if an agent is available
         pass
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
 
         if self._is_agent_available():
             return [SlotSet("is_agent_availble", True)]
