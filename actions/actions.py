d13e294c179298dea83bec9b7013aaf24dc65818
hsm207 <hsm207@users.noreply.github.com>
2021-09-13 19:21:11 +0000

    Setup simple two stage fallback

--- actions/actions.py
+++ actions/actions.py
@@ -1,27 +1,53 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from typing import Any, Dict, List, Text
+from rasa.shared.constants import DEFAULT_NLU_FALLBACK_INTENT_NAME
 
+from rasa.shared.core.constants import ACTION_DEFAULT_ASK_AFFIRMATION_NAME, ACTION_DEFAULT_ASK_REPHRASE_NAME, USER_INTENT_OUT_OF_SCOPE
+from rasa.shared.nlu.constants import INTENT_NAME_KEY, INTENT_RANKING_KEY
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
 
-# This is a simple example for a custom action which utters "Hello World!"
+class ActionDefaultAskAffirmation(Action):
+    def name(self) -> Text:
+        return ACTION_DEFAULT_ASK_AFFIRMATION_NAME
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+
+        latest_message = tracker.latest_message
+
+        intent_to_affirm = latest_message["intent"].get(INTENT_NAME_KEY)
+
+        intent_ranking = latest_message.get(INTENT_RANKING_KEY) or []
+        if (
+            intent_to_affirm == DEFAULT_NLU_FALLBACK_INTENT_NAME
+            and len(intent_ranking) > 1
+        ):
+            intent_to_affirm = intent_ranking[1][INTENT_NAME_KEY]
+
+        affirmation_message = f"I don't understand what you just said\nDid you mean '{intent_to_affirm}'?"
+
+        dispatcher.utter_message(text = affirmation_message, buttons=[
+                {"title": "Yes", "payload": f"/{intent_to_affirm}"},
+                {"title": "No", "payload": f"/{USER_INTENT_OUT_OF_SCOPE}"},
+            ])
+        
+        return []
+
+class ActionDefaultAskRephrase(Action):
+    def name(self) -> Text:
+        return ACTION_DEFAULT_ASK_REPHRASE_NAME
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message("Please rephrase")
+        dispatcher.utter_message("Thanks")
+
+        return []
