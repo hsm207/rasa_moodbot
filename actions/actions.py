312b3627afe2caea6ae05a66bba48b2c80217a82
hsm207 <hsm207@users.noreply.github.com>
2021-10-27 18:02:46 +0000

    Build a simple bot

--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,46 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+import time
+import asyncio
+import logging
+
+class ActionSleepSync(Action):
+
+    def name(self) -> Text:
+        return "action_sleep_sync"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        sleep_time = tracker.slots["sleep_time"]
+
+        logging.info(f"sleeping for {sleep_time} seconds ...")
+
+        time.sleep(sleep_time)
+        dispatcher.utter_message(text=f"I slept for {sleep_time} seconds synchronously!")
+
+        return []
+
+class ActionSleepAsync(Action):
+
+    def name(self) -> Text:
+        return "action_sleep_async"
+
+    async def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        sleep_time = tracker.slots["sleep_time"]
+
+        logging.info(f"sleeping for {sleep_time} seconds ...")
+
+        await asyncio.sleep(sleep_time)
+        dispatcher.utter_message(text=f"I slept for {sleep_time} seconds asynchronously!")
+
+        return []
