190b0dc9780ece5fc23c679ffe9c958eb04c6f29
hsm207 <hsm207@users.noreply.github.com>
2021-11-12 13:04:15 +0000

    Additions to pass test

--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,29 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
+from datetime import datetime
+
+
+class ActionMakeBooking(Action):
+    def name(self) -> Text:
+        return "action_make_booking"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+
+        c = datetime.now()
+        current_date = str(c.date())
+        current_time = str(c.time())
+
+        dispatcher.utter_message(f"Made a booking on {current_date} at {current_time}")
+
+        return [SlotSet("book_date", current_date), SlotSet("book_time", current_time)]
