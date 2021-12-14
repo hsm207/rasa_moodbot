cbcc12f3d5cc410e40a6be0d684a5d2ba9747ac7
hsm207 <hsm207@users.noreply.github.com>
2021-12-14 21:01:04 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Introduction
+
+This repo demonstrates how to use a knowledge graph to disambiguate user input.
+
+# Usage
+
+1. Run `make build` to build the bot
+2. Talk to the bot using the [chat widget](./ui/ui.html)
+3. Run `make clean` to clean up
--- actions/actions.py
+++ actions/actions.py
@@ -1,27 +1,65 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+import logging
+from typing import Any, Dict, List, Text
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.events import SlotSet
+from rasa_sdk.executor import CollectingDispatcher
+from typedb.client import *
 
 
-# This is a simple example for a custom action which utters "Hello World!"
+def get_device_plan_type(device):
+    query = f"""
+    match
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+        $d isa device, has name "{device}";
+        $p isa plan;
+        $r (device:$d, plan:$p) isa contract;
+
+    get $p;
+    """
+
+    with TypeDB.core_client("knowledge-base:1729") as client:
+        with client.session("shop", SessionType.DATA) as session:
+            with session.transaction(TransactionType.READ) as read_transaction:
+                plan = read_transaction.query().match(query)
+                plan = list(plan)
+
+    if not plan:
+        return None
+
+    return plan[0].get("p").get_type().get_label().name()
+
+
+def has_entity_type(entities, type):
+    return any(e for e in entities if e["entity"] == type)
+
+
+def extract_entity(entities, type):
+    return [e["value"] for e in entities if e["entity"] == type][0]
+
+
+class ActionDisambiguateContract(Action):
+    def name(self) -> Text:
+        return "action_disambiguate_contract"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+
+        entities = tracker.latest_message.get("entities")
+
+        has_contract_type = has_entity_type(entities, "contract_type")
+        has_device = has_entity_type(entities, "device")
+
+        if has_contract_type or not has_device:
+            return []
+
+        device = extract_entity(entities, "device")
+        plan_type = get_device_plan_type(device)
+        logging.debug(f"device is {device}")
+        logging.debug(f"plan_type is {plan_type}")
+
+        return [SlotSet("contract_type", plan_type)]
